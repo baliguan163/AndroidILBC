179376eee7f94123952a913122ed72bcd045dac0
Ado <baliguan163@163.com>
2018-08-13 14:13:46 +0800

    Update MainActivity.java

--- src/com/audio/demo/MainActivity.java
+++ src/com/audio/demo/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
-	String LOG = "MainActivity";
+	String LOG = "MainActivity"; //dsdfdsf
 	private AudioWrapper audioWrapper;
 	// View
 	private Button btnStartRecord;
