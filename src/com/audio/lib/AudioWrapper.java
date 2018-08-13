7f40e96714a3cad4b30f91de8d8fb4af964ed871
baliguan163 <baliguan163@163.com>
2018-08-13 13:49:18 +0800

    添加功能

---
+++ src/com/audio/lib/AudioWrapper.java
@@ -0,0 +1,44 @@
+package com.audio.lib;
+
+import com.audio.demo.receiver.AudioReceiver;
+import com.audio.demo.sender.AudioRecorder;
+
+public class AudioWrapper {
+
+	private AudioRecorder audioRecorder;
+	private AudioReceiver audioReceiver;
+	private static AudioWrapper instanceAudioWrapper;
+	private AudioWrapper() {
+	}
+
+	public static AudioWrapper getInstance() {
+		if (null == instanceAudioWrapper) {
+			instanceAudioWrapper = new AudioWrapper();
+		}
+		return instanceAudioWrapper;
+	}
+
+	public void startRecord() {
+		if (null == audioRecorder) {
+			audioRecorder = new AudioRecorder();
+		}
+		audioRecorder.startRecording();
+	}
+
+	public void stopRecord() {
+		if (audioRecorder != null)
+			audioRecorder.stopRecording();
+	}
+
+	public void startListen() {
+		if (null == audioReceiver) {
+			audioReceiver = new AudioReceiver();
+		}
+		audioReceiver.startRecieving();
+	}
+
+	public void stopListen() {
+		if (audioRecorder != null)
+			audioRecorder.stopRecording();
+	}
+}
